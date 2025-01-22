b65e79485f8d49d56ff99a2e6af90a9c05c2609a
Francisco Gemaque <gemaquejr@hotmail.com>
2025-01-21 21:42:18 -0300

    Adiciona auth.routes.ts
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import prisma from './config/db';
+import router from './modules/auth/auth.routes';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(router)
 
 const startServer = async (): Promise<void> => {
   try {
